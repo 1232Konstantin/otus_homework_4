fa09e36e5850de226d98cec66138ae45d0ba3962
dilog777 <dima979837@yandex.ru>
2023-06-11 01:01:06 +0300

    Added doxygen

--- src/library/dummy.cpp
+++ src/library/dummy.cpp
@@ -1,3 +1,8 @@
-// Without this file cmake throws error:
-// >> CMake Error: CMake can not determine linker language for target: print_ip_lib
-// This is due to missing of implementation files (*.cpp) in the library
+/*!
+\file
+\brief Patch file for correct work of smake
+
+Without this file cmake throws error:
+>> CMake Error: CMake can not determine linker language for target: print_ip_lib
+This is due to missing of implementation files (*.cpp) in the library
+*/
--- src/test/StdOutReader.h
+++ src/test/StdOutReader.h
@@ -3,14 +3,35 @@
 #include <sstream>
 #include <iostream>
 
+/*!
+\file
+\brief StdOutReader class header file
+
+This file contains the definition of the StdOutReader class used to intercept messages sent 
+to standard output.
+*/
 
 
+/*!
+	\brief Class for intercepting the standard output stream
+	\author dilog
+	\version 1.0
+	\date 2023.06.11
+	\warning This class can only be instantiated once
+
+	When an instance of this class is created, the standard output stream buffer is replaced. 
+	When the object is destroyed, the standard buffer is returned. The information printed to 
+	the standard output stream can be considered as the readOut function.
+*/
 class StdOutReader
 {
 public:
 	StdOutReader();
 	~StdOutReader();
 
+	/// \brief Get printed text
+	/// \details Reads the text output to the dummy buffer
+	/// \return text of messages sent to standard output
 	std::string readOut();
 
 private:
